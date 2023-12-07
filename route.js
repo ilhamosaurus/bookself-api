5c345f1837c1a61766922bce1fcb0c8589f2e383
Muhammad Ilham Karisma <ilhamsssa@gmail.com>
2023-12-07 14:07:25 +0700

    feat: add route

---
+++ route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const routes = [
+    {
+      path: '/books',
+			method: 'post',
+
+    }
+];
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyPaser = require('body-parser');
+const routes = require('./route');
 const app = express();
 
 const init = async () => {
 	const server = app.listen(3200, 'localhost', () =>
 	console.log('Server running on 3200')
 	);
+
+	app.use(routes);
 
 	server.emit();
 };
